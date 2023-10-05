b397d5a97c0c06bac654f6ab655e95793a769175
Rahul Dey <rahuldey832000@gmail.com>
2023-10-05 20:15:04 +0530

    Create Breakfast Controller

---
+++ ArtisanAura.Api/Controllers/BreakfastController.cs
@@ -0,0 +1,34 @@
+using ArtisanAura.Contracts.Breakfast;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ArtisanAura.Api.Controllers
+{
+    [ApiController]
+    [Route("api/breakfast")]
+    public class BreakfastController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpsertBreakfast(Guid id, UpsertBreakfastRequest request)
+        {
+            return Ok(request);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
